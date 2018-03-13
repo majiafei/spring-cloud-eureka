f727752f47bc63c19988d8c7c895af4e02bd15d3
majiafei <majiafei@dafycredit.com>
2018-03-13 13:58:14 +0800

    new project

---
+++ src/main/java/org/giveucredit/springcloud/euraka/mjf/SpringCloudEurakaMain.java
@@ -0,0 +1,18 @@
+package org.giveucredit.springcloud.euraka.mjf;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurakaMain {
+
+	private static Logger logger = LoggerFactory.getLogger(SpringCloudEurakaMain.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurakaMain.class, args);
+		logger.info("SpringCloudEurakaMain start success");
+	}
+	
+}
